e9bfa068b1a50b39991c1d7312c46caa15af5092
George Hickman <george@ghickman.co.uk>
2023-06-02 12:48:52 +0100

    update all the things

--- update_plex.py
+++ update_plex.py
@@ -6,42 +6,54 @@ import xml.etree.cElementTree as etree
 import click
 import requests
 
+config_path = "~/.config/update_plex"
+
+s = requests.Session()
+
+
+def get_config(key):
+    config = configparser.ConfigParser()
+    config.read(os.path.expanduser(config_path))
+
+    return config["DEFAULT"][key]
+
+
+def exit_with_error(msg):
+    click.echo(
+        f'{msg} Have you created the config file in "{config_path}"?',
+        err=True,
+    )
+    sys.exit(1)
+
 
 @click.command()
-@click.option('--host')
+@click.option("--host")
+@click.option("--token")
 @click.version_option()
-def run(host):
+def run(host, token):
     if not host:
-        path = '~/.config/update_plex'
-        config = configparser.ConfigParser()
-        config.read(os.path.expanduser(path))
         try:
-            host = config['DEFAULT']['host']
+            host = get_config("host")
         except KeyError:
-            msg = 'Don\'t know where plex is. Have you created the config file in "{}"?'
-            click.echo(msg.format(path), err=True)
-            sys.exit(1)
+            exit_with_error("Don't know where plex is.")
 
+    if not token:
         try:
-            token = config['DEFAULT']['token']
+            token = get_config("token")
         except KeyError:
-            msg = 'Can\'t authenticate without a token. Have you created the config file in "{}"?'
-            click.echo(msg.format(path), err=True)
-            sys.exit(1)
+            exit_with_error("Can't authenticate without a token.")
 
-    url = 'http://{}/library/sections'.format(host)
+    url = f"https://{host}/library/sections"
 
-    s = requests.Session()
-    s.headers = {'X-Plex-Token': token}
+    s.headers = {"X-Plex-Token": token}
     r = s.get(url)
     r.raise_for_status()
 
     path = './/Directory[@type="show"]'
     for section in etree.fromstring(r.content).findall(path):
-        url = '{}/{}/refresh'.format(url, section.attrib['key'])
-        r = s.get(url)
+        r = s.get(f"{url}/{section.attrib['key']}/refresh")
         r.raise_for_status()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
